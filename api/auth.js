c60cefd91bad663bc9336d3bce8bc16ed30de58c
juancedenotavera <juancedeno.tavera@gmail.com>
2026-09-06 13:44:47 -0400

    Change API key to use SUPABASE_PUBLISHABLE_KEY

--- api/auth.js
+++ api/auth.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "apikey": process.env.SUPABASE_ANON_KEY
+        apikey: process.env.SUPABASE_PUBLISHABLE_KEY
         },
         body: JSON.stringify({
           email,
